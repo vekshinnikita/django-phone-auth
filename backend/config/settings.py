be0ab009e252a62a0fbb7cc225b9ffbfc9182be7
Nikita <vekshinnikita>
2024-04-28 23:01:09 +0300

    add static

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -127,7 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 VENV_PATH = os.path.dirname(BASE_DIR)
 STATIC_ROOT = os.path.join(VENV_PATH, 'static')
