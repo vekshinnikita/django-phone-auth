b02c7fe919c298f7eae57698e7e422c4c3783686
Nikita <vekshinnikita>
2024-04-28 22:44:47 +0300

    fix start

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000 
+gunicorn config.wsgi:application --bind 0.0.0.0:8000 
